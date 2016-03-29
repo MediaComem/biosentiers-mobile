8305790d23d2107d5691894abef6f38aea0e912a
Tazaf <mathias.oberson@heig-vd.ch>
2016-03-29 17:33:37 +0200

    Utilisation d'une popup pour la validation des données du QR code

--- www/js/controllers/AuthCtrl.js
+++ www/js/controllers/AuthCtrl.js
@@ -1,47 +1,48 @@
 /**
  * Created by Mathias on 29.03.2016.
  */
-app.controller('AuthCtrl', function ($scope, $state, AR, $cordovaBarcodeScanner, $ionicPlatform, $ionicModal) {
+app.controller('AuthCtrl', function ($scope, $state, AR, $cordovaBarcodeScanner, $ionicPlatform, $ionicPopup) {
   $scope.infos = {};
-
-  $scope.closeModal = function () {
-    $scope.modal.hide();
-  };
-
-  $scope.validateQR = function () {
-    console.log('validateQR');
-    $state.go('app.playlists');
-    $scope.modal.hide();
-  };
-
-  $scope.$on('$destroy', function () {
-    $scope.modal.remove();
-  });
 
   $ionicPlatform.ready(function () {
 
     $scope.doAccountLogin = function () {
-      $state.go('app.playlists');
+      $state.go('app.outings');
     };
 
     $scope.doQRCodeLogin = function () {
-
-      $ionicModal.fromTemplateUrl('templates/modals/qr-overview.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-      }).then(function (modal) {
-        $scope.modal = modal;
-      });
 
       $cordovaBarcodeScanner
         .scan()
         .then(function (data) {
           console.log(angular.fromJson(data.text));
           $scope.infos = angular.fromJson(data.text);
-          $scope.modal.show();
+          showQRValidation();
         }, function (error) {
           console.log(error);
         })
     };
-  })
+  });
+
+  var showQRValidation = function () {
+    $ionicPopup.show({
+      title: "Validation",
+      templateUrl: 'templates/popups/qr-overview.html',
+      scope: $scope,
+      buttons: [
+        {
+          text: "Pas du tout",
+          type: "button-assertive"
+        },
+        {
+          text: "C'est ça !",
+          type: "button-balanced",
+          onTap: function (e) {
+            $state.go('app.outings');
+            return true;
+          }
+        }
+      ]
+    });
+  }
 });
