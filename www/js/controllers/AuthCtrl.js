621357d315b936001c04de8a73e7e469e918edfd
Tazaf <mathias.oberson@heig-vd.ch>
2016-03-29 16:01:26 +0200

    Connexion avec QR Code (Stub) - utilisation des modals - màj

--- www/js/controllers/AuthCtrl.js
+++ www/js/controllers/AuthCtrl.js
@@ -1,20 +1,44 @@
 /**
  * Created by Mathias on 29.03.2016.
  */
-app.controller('AuthCtrl', function ($scope, $state, AR, $cordovaBarcodeScanner, $ionicPlatform) {
+app.controller('AuthCtrl', function ($scope, $state, AR, $cordovaBarcodeScanner, $ionicPlatform, $ionicModal) {
+  $scope.infos = {};
+
+  $scope.closeModal = function () {
+    $scope.modal.hide();
+  };
+
+  $scope.validateQR = function () {
+    console.log('validateQR');
+    $state.go('app.playlists');
+    $scope.modal.hide();
+  };
+
+  $scope.$on('$destroy', function () {
+    $scope.modal.remove();
+  });
+
   $ionicPlatform.ready(function () {
-    console.log('plateforme prête');
+
     $scope.doAccountLogin = function () {
-      console.log('Account Login');
       $state.go('app.playlists');
     };
+
     $scope.doQRCodeLogin = function () {
-      console.log('QR Code Login');
+
+      $ionicModal.fromTemplateUrl('templates/modals/qr-overview.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function (modal) {
+        $scope.modal = modal;
+      });
+
       $cordovaBarcodeScanner
         .scan()
         .then(function (data) {
-          console.log(data);
-          $state.go('app.playlists');
+          console.log(angular.fromJson(data.text));
+          $scope.infos = angular.fromJson(data.text);
+          $scope.modal.show();
         }, function (error) {
           console.log(error);
         })
--- www/js/services/ar.js
+++ www/js/services/ar.js
@@ -2,7 +2,7 @@
  * Created by Mathias on 29.03.2016.
  */
 app.service('AR', function() {
-  this.url = 'ar/index.html';
+  this.url = 'wikitude/index.html';
   this.fonction = function() {
     console.log('fonction AR');
   }
