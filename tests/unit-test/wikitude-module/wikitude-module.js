74bbc246cb578a6bc8ead330b238be5b099a4038
Tazaf <obersonmathias@gmail.com>
2016-04-04 19:42:10 +0200

    Suite des tests de Unit Test sur le module wikitude

--- tests/unit-test/wikitude-module/wikitude-module.js
+++ tests/unit-test/wikitude-module/wikitude-module.js
@@ -2,20 +2,74 @@
  * Created by Mathias on 04.04.2016.
  */
 describe('WikitudeModule', function () {
-  describe('Wikitude service', function () {
-    beforeEach(module('WikitudeModule'));
+	describe('Wikitude service', function () {
+		var service, WikitudeFunctionsMock;
+		beforeEach(module('WikitudeModule'));
 
-    it('should have an \'init\' property that is a method', inject(function (Wikitude) {
-      expect(Wikitude.init).toBeDefined();
-      expect(Wikitude.init).toEqual(jasmine.any(Function));
-    }));
+		beforeEach(function () {
+			WikitudeFunctionsMock = {
+				foo: jasmine.createSpy('foo')
+			};
 
-    xdescribe('executeARViewCall()', function () {
-      it('should only accepts \'architectsdk://\' calls', inject(function (Wikitude) {
-        expect(function(){Wikitude.executeARViewCall('foo')}).toThrowError(SyntaxError);
-        expect(function(){Wikitude.executeARViewCall('architectsdk://foo?bar')}).toThrowError(SyntaxError);
-        expect(function(){Wikitude.executeARViewCall('architectsdk://foo')}).toThrowError(TypeError);
-      }))
-    })
-  })
+			module(function ($provide) {
+				$provide.value('WikitudeFunctions', WikitudeFunctionsMock);
+			})
+		});
+
+
+		beforeEach(inject(function (Wikitude) {
+			service = Wikitude;
+		}));
+
+		it('should have an \'init\' property that is a method', function() {
+			expect(service.init).toBeDefined();
+			expect(service.init).toEqual(jasmine.any(Function));
+		});
+
+		describe('parseActionUrl()', function () {
+			it('should return object with function and parameters properties from url', function () {
+				var url = 'architectsdk://foo?{"bar":"foobar"}';
+				var object = {
+					funcName: 'foo',
+					parameters: {
+						bar: 'foobar'
+					}
+				};
+				var result = service.parseActionUrl(url);
+				expect(result).toEqual(object);
+			});
+
+			it('should return object with function property from url', function() {
+				var url = 'architectsdk://foo';
+				var result = service.parseActionUrl(url);
+				var object = {
+					funcName: 'foo',
+					parameters: null
+				};
+				expect(result).toEqual(object);
+			});
+
+			it('should throw an error because of a bad json parameter after the \'?\'', function () {
+				var url = 'architectsdk://foo?bar';
+				expect(function(){service.parseActionUrl(url)}).toThrowError(SyntaxError);
+			});
+
+			it('should throw an error because of a badly formed url', function () {
+				var url = 'foo';
+				expect(function(){service.parseActionUrl(url)}).toThrowError(SyntaxError);
+			})
+		});
+
+		describe('executeActionCall()', function () {
+			it('should call the foo function', function () {
+				var url = 'architectsdk://foo';
+				service.executeActionCall(url);
+				expect(WikitudeFunctionsMock.foo).toHaveBeenCalled();
+			});
+			it('should throw an exception caused by the missing bar function', function () {
+				var url = 'architectsdk://bar';
+				expect(function(){service.executeActionCall(url)}).toThrowError(TypeError);
+			});
+		});
+	})
 });
--- www/app/wikitude-module/wikitude.service.js
+++ www/app/wikitude-module/wikitude.service.js
@@ -2,161 +2,167 @@
  * Created by Mathias on 29.03.2016.
  */
 (function () {
-  'use strict';
+	'use strict';
 
-  angular
-    .module('WikitudeModule')
-    .factory('Wikitude', ARFactory);
+	angular
+		.module('WikitudeModule')
+		.factory('Wikitude', ARFactory);
 
-  //function ARService() {
-  //  var plugin = cordova.require('com.wikitude.phonegap.WikitudePlugin.WikitudePlugin');
-  //  this.result = function () {
-  //    console.log(plugin);
-  //  }
-  //}
-  //
-  //function ARProvider() {
-  //  var service = {
-  //    plugin: null,
-  //    result: result
-  //  };
-  //
-  //  this.loadPlugin = loadPlugin;
-  //  this.$get = $get;
-  //
-  //  ////////////////////
-  //
-  //  function result() {
-  //    console.log(service.plugin);
-  //  }
-  //
-  //  function $get() {
-  //    return service;
-  //  }
-  //
-  //  function loadPlugin() {
-  //    service.plugin = cordova.require('com.wikitude.phonegap.WikitudePlugin.WikitudePlugin');
-  //  }
-  //}
+	//function ARService() {
+	//  var plugin = cordova.require('com.wikitude.phonegap.WikitudePlugin.WikitudePlugin');
+	//  this.result = function () {
+	//    console.log(plugin);
+	//  }
+	//}
+	//
+	//function ARProvider() {
+	//  var service = {
+	//    plugin: null,
+	//    result: result
+	//  };
+	//
+	//  this.loadPlugin = loadPlugin;
+	//  this.$get = $get;
+	//
+	//  ////////////////////
+	//
+	//  function result() {
+	//    console.log(service.plugin);
+	//  }
+	//
+	//  function $get() {
+	//    return service;
+	//  }
+	//
+	//  function loadPlugin() {
+	//    service.plugin = cordova.require('com.wikitude.phonegap.WikitudePlugin.WikitudePlugin');
+	//  }
+	//}
 
-  function ARFactory(WikitudeFunctions) {
+	function ARFactory(WikitudeFunctions) {
 
-    var plugin = null;
-    var protocol = 'architectsdk://';
+		var plugin = null;
+		var protocol = 'architectsdk://';
 
-    var service = {
-      config: {camera_position: 'back'},
-      deviceSupportsFeatures: null,
-      reqFeatures: ['geo', '2dtracking'],
-      url: 'wikitude-app/index.html',
-      init: init,
-      executeARViewCall: executeARViewCall,
-      getPlugin: getPlugin,
-      launchAR: launchAR
-    };
+		var service = {
+			config: {camera_position: 'back'},
+			deviceSupportsFeatures: null,
+			reqFeatures: ['geo', '2dtracking'],
+			url: 'wikitude-app/index.html',
+			init: init,
+			executeActionCall: executeActionCall,
+			getPlugin: getPlugin,
+			launchAR: launchAR,
+			parseActionUrl: parseActionUrl
+		};
 
-    return service;
+		return service;
 
-    ////////////////////
+		////////////////////
 
-    function launchAR() {
-      console.log(plugin);
-    }
+		function launchAR() {
+			console.log(plugin);
+		}
 
-    /**
-     * This function should be used to retrieve the Wikitude plugin in order to access and/or execute any of it's built-in functions.
-     * It follows the Singleton pattern in that it either load the plugin and returns it if it's the first time it's called,
-     * or return the already loaded plugin for each subsequent call.
-     * @returns {WikitudePlugin} The cordova Wikitude plugin
-     */
-    function getPlugin() {
-      if (plugin === null) {
-        plugin = cordova.require('com.wikitude.phonegap.WikitudePlugin.WikitudePlugin');
-      }
-      return plugin;
-    }
+		/**
+		 * This function should be used to retrieve the Wikitude plugin in order to access and/or execute any of it's built-in functions.
+		 * It follows the Singleton pattern in that it either load the plugin and returns it if it's the first time it's called,
+		 * or return the already loaded plugin for each subsequent call.
+		 * @returns {WikitudePlugin} The cordova Wikitude plugin
+		 */
+		function getPlugin() {
+			if (plugin === null) {
+				plugin = cordova.require('com.wikitude.phonegap.WikitudePlugin.WikitudePlugin');
+			}
+			return plugin;
+		}
 
-    /**
-     * Checks if the device supports the features needed by the ARchitect World.
-     * These features are set with the reqFeatures property of the Wikitude service.
-     * The result of this check is available through the deviceSupportsFeatures property of the Wikitude service
-     * for it to be used later (alerting the user that he's/she's device is not compatible, for instance).
-     * For conveniency, the result of the check is returned by the function.
-     * @returns {Boolean} TRUE if the device supports every features, FALSE if at least one feature is not supported
-     */
-    function checkDevice() {
-      getPlugin().isDeviceSupported(function () {
-        service.deviceSupportsFeatures = true;
-      }, function () {
-        service.deviceSupportsFeatures = false;
-      }, service.reqFeatures);
-      return service.deviceSupportsFeatures;
-    }
+		/**
+		 * Checks if the device supports the features needed by the ARchitect World.
+		 * These features are set with the reqFeatures property of the Wikitude service.
+		 * The result of this check is available through the deviceSupportsFeatures property of the Wikitude service
+		 * for it to be used later (alerting the user that he's/she's device is not compatible, for instance).
+		 * For conveniency, the result of the check is returned by the function.
+		 * @returns {Boolean} TRUE if the device supports every features, FALSE if at least one feature is not supported
+		 */
+		function checkDevice() {
+			getPlugin().isDeviceSupported(function () {
+				service.deviceSupportsFeatures = true;
+			}, function () {
+				service.deviceSupportsFeatures = false;
+			}, service.reqFeatures);
+			return service.deviceSupportsFeatures;
+		}
 
-    /**
-     * This function should be set, with the setOnUrlInvokeCallback method, as the callback function for the 'architectsdk://' call done by the Wikitude AR View.
-     * The string passed as the 'call' argument must be formatted as follow :
-     * 'architectsdk://name_of_the_function_to_execute[?json_object_containging_any_function_parameters]'
-     * Example :
-     * 'architectsdk://saveClient?{"firstName":"Foo","lastName":"Bar"}'
-     * This call will execute the saveClient function, passing in an object, containing a firstName and a lastName property, as the parameter.
-     * @param call A string starting with 'architectsdk://'.
-     */
-    function executeARViewCall(call) {
-      if (call.substr(0, protocol.length) === protocol) {
-        var action = call.substr(protocol.length);
-        // When the called function has no parameter (there's no '?' in the call string)
-        if (action.indexOf('?') === -1) {
-          WikitudeFunctions[action]();
-        } else {
-          try {
-            // When the called function has a parameter (there's a '?' in the call string)
-            var fn = action.substr(0, i);
-            var param = action.substr(i + 1);
-            WikitudeFunctions[fn](angular.fromJson(param));
-          } catch (e) {
-            // Exception handling
-            if (e instanceof SyntaxError) {
-              throw new SyntaxError('executeARViewCall() expects the substring following the \'?\' in the parameter string to be a valid JSON object. \'' + param + '\' given.');
-            } else if (e instanceof TypeError) {
-              throw new TypeError('There is no function named \'' + fn + '\' defined in the WikitudeFunctions service.');
-            } else {
-              throw e;
-            }
-          }
-        }
-      } else {
-        throw new SyntaxError('executeARViewCall() expects first parameter to be a string starting with \'architectsdk://\'.');
-      }
-    }
+		/**
+		 * This function should be set, with the setOnUrlInvokeCallback method, as the callback function for the 'architectsdk://' call done by the Wikitude AR View.
+		 * The string passed as the 'call' argument must be formatted as follow :
+		 * 'architectsdk://name_of_the_function_to_execute[?json_object_containging_any_function_parameters]'
+		 * Example :
+		 * 'architectsdk://saveClient?{"firstName":"Foo","lastName":"Bar"}'
+		 * This call will execute the saveClient function, passing in an object, containing a firstName and a lastName property, as the parameter.
+		 * @param call A string starting with 'architectsdk://'.
+		 */
+		function executeActionCall(call) {
+			var action = parseActionUrl(call);
+			try {
+				WikitudeFunctions[action.funcName](action.parameters);
+			} catch (e) {
+				if (e instanceof TypeError) {
+					throw new TypeError('There is no defined function named \'' + action.funcName + '\' in the Wikitude service. Please check the name or create it.');
+				} else {
+					throw e;
+				}
+			}
+		}
 
-    function parseActionUrl(url) {
-      var action = {};
-      if (url.substr(0, protocol.length) === protocol) {
-        var call = url.substr(protocol.length);
-        if (call.indexOf('?') === -1) {
-          action.fn = call;
-        } else {
-          action.function = action.substr(0, i);
-          action.parameters = angular.fromJson(action.substr(i + 1));
-        }
-      } else {
-        throw new SyntaxError('executeARViewCall() expects first parameter to be a string starting with \'architectsdk://\'.');
-      }
-    }
+		/**
+		 * Parses an url starting with 'architectsdk://' and creates an action object.
+		 * This object will have two properties : funcName and parameters containing, respectively,
+		 * the name of the function that will be called by executeActionCall() and the parameters to this function, if provided.
+		 *
+		 * @param url
+		 * @return {Object}
+		 */
+		function parseActionUrl(url) {
+			var action = {};
+			if (url.substr(0, protocol.length) === protocol) {
+				var call = url.substr(protocol.length);
+				var i = call.indexOf('?');
+				if (i === -1) {
+					action.funcName = call;
+					action.parameters = null;
+				} else {
+					try {
+						action.funcName = call.substr(0, i);
+						var param_string = call.substr(i + 1);
+						action.parameters = angular.fromJson(param_string);
+					} catch (e) {
+						if (e instanceof SyntaxError) {
+							throw new SyntaxError('executeActionCall() expects the substring following the \'?\' in the parameter string to be a valid JSON object. \'' + param_string + '\' given.');
+						} else {
+							throw e;
+						}
+					}
+				}
+				return action;
+			} else {
+				throw new SyntaxError('executeActionCall() expects first parameter to be a string starting with \'architectsdk://\'.');
+			}
+		}
 
-    /**
-     * Initializes the plugin by calling checkDevice() first, to check if the device supports the requested features.
-     * Then, it sets the executeARViewCall function as the callback to call every time the AR View uses a document.location = 'architectsdk://foo?bar' call.
-     * This way, you don't have to set a process to catch and analyze those calls, as it is done for you with the executeARViewCall function.
-     * Check it's documentation to see how it's done.
-     */
-    function init() {
-      console.log('init started');
-      checkDevice();
-      console.log('device checked');
-      getPlugin().setOnUrlInvokeCallback(executeARViewCall);
-      console.log('callback set');
-    }
-  }
+		/**
+		 * Initializes the plugin by calling checkDevice() first, to check if the device supports the requested features.
+		 * Then, it sets the executeActionCall function as the callback to call every time the AR View uses a document.location = 'architectsdk://foo?bar' call.
+		 * This way, you don't have to set a process to catch and analyze those calls, as it is done for you with the executeActionCall function.
+		 * Check it's documentation to see how it's done.
+		 */
+		function init() {
+			console.log('init started');
+			checkDevice();
+			console.log('device checked');
+			getPlugin().setOnUrlInvokeCallback(executeActionCall);
+			console.log('callback set');
+		}
+	}
 })();
