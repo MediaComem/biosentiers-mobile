f6a9ce6cc5175fcadb0a7b45adce271ce560e95c
sarah <sarah.composto@heig-vd.ch>
2016-07-04 15:03:11 +0200

    Add swipe between tabs

--- www/app/outings/outings.controller.js
+++ www/app/outings/outings.controller.js
@@ -6,11 +6,26 @@
 
   angular
     .module('app')
-    .controller('OutingsCtrl', OutingsCtrl)
+    .controller('OutingsCtrl', OutingsCtrl);
 
-  function OutingsCtrl(outingsData) {
+  function OutingsCtrl(outingsData, $scope, $ionicTabsDelegate) {
     var ctrl = this;
     ctrl.outings = outingsData;
     console.log(ctrl.outings);
+  
+    $scope.goForward = function () {
+        var selected = $ionicTabsDelegate.selectedIndex();
+        if (selected != -1) {
+            $ionicTabsDelegate.select(selected + 1);
+        }
+    }
+
+    $scope.goBack = function () {
+        var selected = $ionicTabsDelegate.selectedIndex();
+        if (selected != -1 && selected != 0) {
+            $ionicTabsDelegate.select(selected - 1);
+        }
+    }
+    
   }
 })();
