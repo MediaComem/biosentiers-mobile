dbe21f8c6496a947d6b86a7773f76ede6a550c25
Mathias Oberson <mathias.oberson@heig-vd.ch>
2016-11-18 15:04:01 +0100

    Added the pause/resume behavior when showing and hiding/removing modals.

--- www/wikitude-worlds/main/modals/modals.service.js
+++ www/wikitude-worlds/main/modals/modals.service.js
@@ -2,61 +2,67 @@
  * Created by Mathias on 31.08.2016.
  */
 (function () {
-  'use strict';
-  angular
-    .module('modals')
-    .factory('Modals', ModalsService);
+	'use strict';
+	angular
+		.module('modals')
+		.factory('Modals', ModalsService);
 
-  function ModalsService($q) {
-    var service = {
-      show  : showModal,
-      hide  : hideModal,
-      remove: removeModal
-    };
+	function ModalsService($q, ArView) {
+		var service = {
+			showCurrent  : showModal,
+			hideCurrent  : hideModal,
+			removeCurrent: removeModal,
+			get isCurrentShown() {return current ? current.isShown : false;}
+		};
 
-    var current = null;
+		var current = null;
 
-    return service;
+		return service;
 
-    ////////////////////
+		////////////////////
 
-    /**
-     * Sets the received modal as the current modal, and opens it.
-     * @param modal The modal to set as current.
-     */
-    function showModal(modal) {
-      if (modal) {
-        current = modal;
-        return current.show();
-      } else {
-        return $q.reject('No modal to show');
-      }
-    }
+		/**
+		 * Sets the received modal as the current modal, and opens it.
+		 * @param modal The modal to set as current.
+		 */
+		function showModal(modal) {
+			if (modal) {
+				return modal.show().then(function () {
+					current = modal;
+					ArView.pauseAr();
+					console.log(current);
+				});
+			} else {
+				return $q.reject('No modal to show');
+			}
+		}
 
-    /**
-     * Hides the BigMapModal, providing that it exists.
-     * @returns Promise
-     */
-    function hideModal() {
-      if (current !== null) {
-        return current.hide();
-      } else {
-        return $q.reject('No active modal to close');
-      }
-    }
+		/**
+		 * Hides the current, providing that it exists.
+		 * @returns Promise
+		 */
+		function hideModal() {
+			if (current !== null) {
+				ArView.resumeAr();
+				return current.hide();
+			} else {
+				return $q.reject('No active modal to close');
+			}
+		}
 
-    /**
-     * Removes the modal, providing that it exists
-     * @returns Promise
-     */
-    function removeModal() {
-      if (current !== null) {
-        return current.remove();
-      } else {
-        return $q.reject('No active modal to remove');
-      }
-    }
+		/**
+		 * Removes the current modal, providing that it exists
+		 * @returns Promise
+		 */
+		function removeModal() {
+			if (current !== null) {
+				ArView.resumeAr();
+				return current.remove();
+			} else {
+				return $q.reject('No active modal to remove');
+			}
+		}
 
-  }
+	}
 })
 ();
