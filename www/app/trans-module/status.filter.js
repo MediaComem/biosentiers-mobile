c3b3f2cc4a78bd22c6b5679d43748c1d2af281ab
Mathias Oberson <mathias.oberson@heig-vd.ch>
2017-02-08 12:04:10 +0100

    Created a custom 'status' filter. It translates the English status values to French one, for displaying.

---
+++ www/app/trans-module/status.filter.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Mathias Oberson on 08.02.2017.
+ */
+(function() {
+  'use strict';
+  angular
+    .module('trans')
+    .filter('status', status);
+
+  function status() {
+    return function(input) {
+      switch (input) {
+        case 'pending':
+          return 'En attente';
+        case 'ongoing' :
+          return 'En cours';
+        case 'finished':
+          return 'Terminée';
+        default:
+          return '';
+      }
+    }
+  }
+})();
---
+++ www/app/trans-module/trans.module.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Mathias Oberson on 08.02.2017.
+ */
+(function() {
+  'use strict';
+  angular.module('trans', []);
+})();
