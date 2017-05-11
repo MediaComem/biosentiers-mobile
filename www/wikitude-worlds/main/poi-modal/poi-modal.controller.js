17477f8233e67dbb3bbbdce4fb417f179b070078
Mathias Oberson <mathias.oberson@heig-vd.ch>
2017-05-11 16:43:58 +0200

    Working on changing the language for the common name of a POI

--- www/wikitude-worlds/main/poi-modal/poi-modal.controller.js
+++ www/wikitude-worlds/main/poi-modal/poi-modal.controller.js
@@ -10,18 +10,36 @@
   function PoiModalCtrlFn(ArView, Excursion, $log, Modals) {
     var poiCtrl = this;
 
+    poiCtrl.selectedLanguage = 'fr';
     poiCtrl.remove = Modals.removeCurrent;
     poiCtrl.setPoiSeen = setPoiSeen;
     poiCtrl.openInBrowser = openInBrowser;
-    poiCtrl.barClassForTheme = barClassForTheme;
+    poiCtrl.classFromTheme = classFromTheme;
+    poiCtrl.updateCommomName = updateCommomName;
+    poiCtrl.isSelectedLanguage = function(lang) {
+      $log.log('PoiModalCtrl:isSelectedLanguage', lang, lang === 'fr');
+      return lang === 'fr';
+    };
+
+    poiCtrl.getImageSource = function(id) {
+      var url = "./assets/img/" + id + "a.jpg";
+      $log.log('PoiModalCtrl:getImageSource', id, url);
+      return url;
+    };
 
     Excursion.currentPoiChangeObs.subscribe(function(data) {
       $log.log('PoiModal:currentPoiChangeObs', data);
       poiCtrl.poi = data.poi;
+      poiCtrl.commonNameLanguages = Object.keys(poiCtrl.poi.properties.common_name);
+      $log.log('PoiModalCtrl:poiCtrl.commonNameLanguages', poiCtrl.commonNameLanguages);
       poiCtrl.content = data.details;
     });
 
     ////////////////////
+
+    function updateCommomName() {
+      $log.log('PoiModalCtrl:updateCommonName', poiCtrl.selectedLanguage);
+    }
 
     function setPoiSeen() {
       Modals.removeCurrent().then(function() {
@@ -33,12 +51,12 @@
       AR.context.openInBrowser(poiCtrl.content.website, true);
     }
 
-    function barClassForTheme(theme) {
+    function classFromTheme(theme) {
       var classes = {
-        bird: 'bar-positive',
-        flower: 'bar-energized',
-        butterfly: 'bar-balanced',
-        tree: 'bar-assertive'
+        bird     : 'positive',
+        flower   : 'energized',
+        butterfly: 'balanced',
+        tree     : 'assertive'
       };
 
       return classes[theme];
